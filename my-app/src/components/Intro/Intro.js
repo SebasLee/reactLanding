74d03a9b2aa09c3a33862835a0384d2d2280e1ba
SebasLee <heechanlee98@gmail.com>
2020-11-17 15:43:29 -0800

    progress

--- my-app/src/components/Intro/Intro.js
+++ my-app/src/components/Intro/Intro.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import './Intro.css';
 import LA from '../../images/LASkyline.jpg';
 
 function Intro() {
     return (
-        <img src={LA} alt="Los Angeles" />
+      <section>
+            <div class="losangeles">
+                 <img src={LA} alt="Los Angeles" />
+            </div>  
+            <h1>LA Travel</h1>
+      </section>  
     );
 }
 
