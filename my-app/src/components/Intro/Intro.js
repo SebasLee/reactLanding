96b552461ca30bf3796c41fa7a2c0e2b3cd1c114
SebasLee <heechanlee98@gmail.com>
2020-11-17 12:16:37 -0800

    started

---
+++ my-app/src/components/Intro/Intro.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import LA from '../../images/LASkyline.jpg';
+
+function Intro() {
+    return (
+        <img src={LA} alt="Los Angeles" />
+    );
+}
+
+export default Intro;
